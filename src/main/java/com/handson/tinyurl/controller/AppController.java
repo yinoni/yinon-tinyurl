358d3d409e0b7dcd708dbd40d76c0cb0d74c3581
yinoni <yinonmegidesh2212@gmail.com>
2026-01-21 13:35:03 +0200

    commit with redis

--- src/main/java/com/handson/tinyurl/controller/AppController.java
+++ src/main/java/com/handson/tinyurl/controller/AppController.java
@@ -1,15 +1,28 @@
 package com.handson.tinyurl.controller;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.handson.tinyurl.service.Redis;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AppController {
+    @Autowired
+    Redis redis;
 
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String getClicks(@PathVariable String tiny) {
-        return "hello";
+    @RequestMapping(value = "/set", method = RequestMethod.GET)
+    public Boolean set(@RequestParam String key,  @RequestParam String value) {
+        return redis.set(key,value);
     }
+
+    @RequestMapping(value = "/get", method = RequestMethod.GET)
+    public String get(@RequestParam String key) {
+        try{
+            return redis.get(key).toString();
+        }
+        catch (Exception e){
+            return e.getMessage();
+        }
+
+    }
+
 }
